826293eae1229026556ed18a9f77df2ede15c44f
Nicolas Missika <nmissika@synthesio.com>
2023-03-16 10:33:03 +0100

    pass 3rd and 4th stages

--- app/server.go
+++ app/server.go
@@ -17,13 +17,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		go handleConn(conn)
 	}
+}
 
+func handleConn(conn net.Conn) {
 	for {
 		buf := make([]byte, 1024)
 		_, err := conn.Read(buf)
@@ -36,16 +41,22 @@ func main() {
 			}
 		}
 
+		fmt.Println("recieved conn")
+
 		_, err = conn.Write([]byte("+PONG\r\n"))
+		fmt.Println("wrote PONG to conn")
+
 		if err != nil {
 			fmt.Println("writing to conn: ", err.Error())
 			os.Exit(1)
 		}
 	}
 
-	err = conn.Close()
+	fmt.Println("closing conn")
+	err := conn.Close()
+
 	if err != nil {
-		fmt.Println("closing con", err.Error())
+		fmt.Println("closing conn: ", err.Error())
 		os.Exit(1)
 	}
 }
